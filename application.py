4cd13627abf7947753cf5c9c4a595eae2ce768c9
egemnoztrk <66872413+egemnoztrk@users.noreply.github.com>
2021-05-02 23:24:40 +0300

    Update application.py

--- application.py
+++ application.py
@@ -10,9 +10,9 @@ application = flask.Flask(__name__)
 q_client_mongo = pymongo.MongoClient("mongodb+srv://egemen:12345@cluster0.5dvoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 mongoDB = q_client_mongo.API
 
-CORS(application)
+CORS(application, supports_credentials=True)
 
-application.config.update( DEBUG=False, SECRET_KEY="65465f4a6s54f6as54g6a54ya687ytq9ew841963684", supports_credentials=True )
+application.config.update( DEBUG=False, SECRET_KEY="65465f4a6s54f6as54g6a54ya687ytq9ew841963684")
 
 
 @application.route("/deneme")
