c03d6e6e131cc968e25ea160ba4b7d1a7ba6ea43
egemnoztrk <66872413+egemnoztrk@users.noreply.github.com>
2021-05-02 13:46:31 +0300

    login implementation

--- application.py
+++ application.py
@@ -1,24 +1,93 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request,session
 import pymongo
 import json
 from bson import json_util
+from flask_cors import CORS
+import bcrypt
+from itsdangerous import URLSafeTimedSerializer
 
 application = Flask(__name__)
 q_client_mongo = pymongo.MongoClient("mongodb+srv://egemen:12345@cluster0.5dvoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 mongoDB = q_client_mongo.API
 
+application.secret_key = 'the random string'
+CORS(application)
 
-@application.route("/")
-def index():
-    return "Your Flask App Works!<br>Yarramı ye beşiktaş"
+
 
 @application.route("/user")
 def user():
     inputs = request.args
-    username = inputs["username"]  
-    res =jsonify(json.loads(json.dumps([element for element in mongoDB.Users.find({"USERNAME":username},{"_id": 0,"USERNAME":1,"PASSWORD":1,"ACCOUNT-TYPE":1})], default=json_util.default)))
+    email = inputs["email"]  
+    if "email" in session:
+        res =jsonify(json.loads(json.dumps([element for element in mongoDB.Users.find({"EMAIL":session["email"]},{"_id": 0,"NAME":1,"EMAIL":1,"ACCOUNT_TYPE":1})], default=json_util.default)))
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
+        return res
+    res =jsonify({"status":"Please Login"})
     res.headers.add('Access-Control-Allow-Credentials', 'true')
     return res
+
+
+@application.route("/register", methods=['post', 'get'])
+def register():
+    inputs = request.args
+    name = inputs["name"]
+    surname = inputs["surname"]
+    email = inputs["email"]
+    password = inputs["password"]
+    accountType=inputs["accountType"]
+    email_found = mongoDB.Users.find_one({"EMAIL": email})
+    if email_found:
+        res = jsonify(False)
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
+        return res
+    mongoDB.Users.insert_one({
+        "NAME" : name,
+        "SURNAME":surname,
+        "EMAIL" : email,
+        "PASSWORD" : bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()),
+        "ACCOUNT_TYPE" :accountType
+    })
+    res = jsonify(True)
+    res.headers.add('Access-Control-Allow-Credentials', 'true')
+    return res
+
+
+
+@application.route("/login", methods=["GET"])
+def login():
+    if "email" in session:
+        res=jsonify({"status":"already logged in","email":session['email']})
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
+        return res
+    inputs=request.args
+    email= inputs['email']
+    password=inputs['password']
+    email_found = mongoDB.Users.find_one({"EMAIL": email})
+    if email_found:
+        passwordcheck = email_found['PASSWORD']
+        if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+            session["email"]=email_found['EMAIL']
+            res=jsonify({"status":"already logged in","email":session['email']})
+            res.headers.add('Access-Control-Allow-Credentials', 'true')
+            return res
+    res=jsonify({"status":"Wrong Mail or Password"})
+    res.headers.add('Access-Control-Allow-Credentials', 'true')
+    return res
+
+
+
+@application.route("/logout", methods=["GET"])
+def logout():
+    if "email" in session:
+        session.pop("email", None)
+        res=jsonify("Logged Out")
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
+        return res
+    else:
+        res=jsonify("Logged Out")
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
+        return res
 
 
 if __name__ == "__main__":
