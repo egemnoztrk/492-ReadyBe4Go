c0485403fe700b22c5f3607c38e069e78b0364c9
egemnoztrk <66872413+egemnoztrk@users.noreply.github.com>
2021-05-02 23:51:54 +0300

    Update application.py

--- application.py
+++ application.py
@@ -10,9 +10,9 @@ application = flask.Flask(__name__)
 q_client_mongo = pymongo.MongoClient("mongodb+srv://egemen:12345@cluster0.5dvoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 mongoDB = q_client_mongo.API
 
-CORS(application, supports_credentials=True)
+CORS(application)
 
-application.config.update( DEBUG=False, SECRET_KEY="65465f4a6s54f6as54g6a54ya687ytq9ew841963684")
+application.config.update( DEBUG=False, SECRET_KEY="65465f4a6s54f6as54g6a54ya687ytq9ew841963684", supports_credentials=True)
 
 
 @application.route("/deneme")
@@ -27,6 +27,8 @@ def user():
         res =jsonify(json.loads(json.dumps([element for element in mongoDB.Users.find({"EMAIL":session["email"]},{"_id": 0,"NAME":1,"EMAIL":1,"ACCOUNT_TYPE":1})], default=json_util.default)))
         return res
     res =jsonify({"status":"Please Login"})
+    res.headers.add('Access-Control-Allow-Credentials', 'true')
+
     return res
 
 
@@ -40,6 +42,7 @@ def register():
     email_found = mongoDB.Users.find_one({"EMAIL": email})
     if email_found:
         res = jsonify(False)
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
         return res
     mongoDB.Users.insert_one({
         "NAME" : name,
@@ -48,6 +51,8 @@ def register():
         "ACCOUNT_TYPE" :accountType
     })
     res = jsonify(True)
+    res.headers.add('Access-Control-Allow-Credentials', 'true')
+
     return res
 
 
@@ -56,6 +61,7 @@ def register():
 def login():
     if "email" in session:
         res=jsonify({"status":"already logged in","email":session['email']})
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
         return res
     inputs=request.args
     email= inputs['email']
@@ -66,8 +72,11 @@ def login():
         if passwordcheck==password:
             session["email"]=email
             res=jsonify({"status":"logged in","email":session['email']})
+            res.headers.add('Access-Control-Allow-Credentials', 'true')
             return res
     res=jsonify({"status":"Wrong Mail or Password"})
+    res.headers.add('Access-Control-Allow-Credentials', 'true')
+
     return res
 
 
@@ -77,9 +86,13 @@ def logout():
     if "email" in session:
         session.pop("email", None)
         res=jsonify("Logged Out")
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
+
         return res
     else:
         res=jsonify("Logged Out")
+        res.headers.add('Access-Control-Allow-Credentials', 'true')
+
         return res
 
 
