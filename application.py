224d9d3683a4cb94bebfc7281985a5979e7a545a
egemnoztrk <66872413+egemnoztrk@users.noreply.github.com>
2021-05-15 00:15:11 +0300

    Update application.py

--- application.py
+++ application.py
@@ -27,7 +27,7 @@ def userSettings():
     return res
 
 @application.route("/restaurantSettings", methods=['post', 'get'])
-def userSettings():
+def restaurantSettings():
     inputs = request.args
     email = inputs["email"]
     res=jsonify(json.loads(json.dumps([element for element in mongoDB.Users.find({"EMAIL":email},{"_id": 0,"NAME":1,"DESCRIPTION":1,"PHONE":1,"CITY":1,"ADDRESS":1,"EMAIL":1,"PASSWORD":1,"ACCOUNT_TYPE":1,"RESERVATION_HOURS":1})], default=json_util.default)))
