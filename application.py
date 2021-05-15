29d5fdd14cd312f71222f81dc2a6e54c19e4a669
egemnoztrk <66872413+egemnoztrk@users.noreply.github.com>
2021-05-15 10:18:05 +0300

    Update application.py

--- application.py
+++ application.py
@@ -48,11 +48,12 @@ def restaurantSettingsSave():
     res.headers.add('Access-Control-Allow-Credentials', 'true')
     return res
 
-@application.route("getRestaurants", methods=['post', 'get'])
+@application.route("/getRestaurants", methods=['post', 'get'])
 def getRestaurants():
     res=jsonify(mongoDB.Users.find({"ACCOUNT_TYPE":"Restaurant"},{}))
     res.headers.add('Access-Control-Allow-Credentials', 'true')
     return res
+
 
 @application.route("/userSettingsSave", methods=['post', 'get'])
 def userSettingsSave():
